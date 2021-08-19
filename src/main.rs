f28dfb4c62728f87b50ffdb652a3304cf3795657
manan <manansoni1399@gmail.com>
2021-08-19 16:04:09 +0530

    implement show, adjust buffer

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,14 @@ use std::io::{self, BufRead};
 use std::path::Path;
 use std::cmp;
 
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 #[derive(Debug)]
 struct SubEditor {
     cursor: usize,
@@ -10,14 +18,6 @@ struct SubEditor {
     pre: Vec<char>,
     post: Vec<char>,
     lines: Vec<(usize, usize)>
-}
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
 }
 
 impl SubEditor {
@@ -45,7 +45,10 @@ impl SubEditor {
                 Ok(content) => self.insert_line(&content),
                 Err(_) => ()
             }
-        }      
+        } 
+        self.cursor = 0;
+        self.curr_line = 0;
+        self.adjust_buffer(); 
     }
 
     fn adjust_buffer(&mut self) {
@@ -56,17 +59,13 @@ impl SubEditor {
         else {
             let deficit = if self.cursor > buff_cursor { self.cursor - buff_cursor } else { buff_cursor - self.cursor };
             for _ in 0..deficit {
-                if self.cursor > buff_cursor {
-                    self.post.push(self.pre.pop().unwrap()) 
+                if self.cursor < buff_cursor {
+                    self.post.push(self.pre.pop().unwrap());
                 } else {
-                    self.pre.push(self.post.pop().unwrap()) 
+                    self.pre.push(self.post.pop().unwrap());
                 }
             }
         }
-    }
-
-    fn move_l(&mut self) {
-        self.cursor -= 1;
     }
     fn move_r(&mut self) {
         self.cursor += 1;
@@ -85,25 +84,50 @@ impl SubEditor {
     fn move_ver(&mut self, n: usize) {
     }
 
+    fn move_eol(&mut self) {
+        self.cursor = self.lines[self.curr_line].0 + self.lines[self.curr_line].1;
+    }
+
     fn insert(&mut self, newchar: &char) {
+        self.adjust_buffer();
     }
 
     fn insert_word(&mut self, newword: &str) {
+        self.adjust_buffer();
     }
 
     fn insert_line(&mut self, newline: &str) {
-        self.lines.push((self.cursor, newline.len()));
-        for ch in newline.chars() {
-            self.pre.push(ch);
+        self.adjust_buffer();
+        let linelen = newline.len();
+        if linelen > 0 {
+            self.lines.push((self.cursor, linelen));
+            for ch in newline.chars() {
+                self.pre.push(ch);
+            }
+            self.cursor += linelen;
+            self.curr_line += 1    
         }
-        self.cursor += newline.len();
-        self.curr_line += 1
     }
 
-    fn show(&self) {
-        println!("line: {} | cursor: {}", self.curr_line, self.cursor);
-        println!("pre: {:?}", self.pre);
-        println!("post: {:?}", self.post);
+    fn show(&mut self) {
+        self.adjust_buffer();
+        println!("--- Med: v0.1 ---");
+        for (ind,line) in self.lines.iter().enumerate() {
+            let mut currline = String::new();
+            if ind < self.curr_line {
+                currline = self.pre[self.lines[ind].0 .. self.lines[ind].0+self.lines[ind].1].iter().collect();
+            } else if ind > self.curr_line {
+                let end = self.post.len() - (self.lines[ind].0 - self.cursor);
+                currline = self.post[end-self.lines[ind].1 .. end].iter().rev().collect();
+            } else {
+                let preline:String = self.pre[self.lines[ind].0 .. self.cursor].iter().collect();
+                let start = self.post.len() - self.lines[ind].1 + (self.cursor - self.lines[ind].0);
+                let postline:String = self.post[start .. self.post.len()].iter().rev().collect();
+                currline = format!("{}{}",preline,postline);
+            }
+            println!("{}| {:?}",ind, currline);
+        }
+        println!("-----------------\n");
     }
 }
 
@@ -116,6 +140,5 @@ fn main() {
     for _ in 0..5 {
         editor.move_r();
     }
-    editor.adjust_buffer();
     editor.show();
 }
