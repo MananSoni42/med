c270cfb0fef76ebef47794374649787ae129ce82
manan <manansoni1399@gmail.com>
2021-08-24 12:16:17 +0530

    implement insert, delete

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use std::fs::File;
-use std::io::{self, BufRead};
 use std::path::Path;
 use std::cmp;
+use std::io::{self, stdout, Write, BufRead};
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    ExecutableCommand, Result,
+    event,
+};
 
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
@@ -67,46 +73,102 @@ impl SubEditor {
             }
         }
     }
-    fn move_r(&mut self) {
-        self.cursor += 1;
-    }
-    fn move_u(&mut self) {
-        self.curr_line = cmp::max(0, self.curr_line-1);
-        self.cursor = self.lines[self.curr_line].0
-    }
-    fn move_d(&mut self) {
-        self.curr_line = cmp::min(self.lines.len()-1, self.curr_line+1);
-        self.cursor = self.lines[self.curr_line].0
+
+    fn move_hor(&mut self, n: i32) {
+        let min_cursor = self.lines[self.curr_line].0;
+        let max_cursor = min_cursor + self.lines[self.curr_line].1;
+        if n > 0 {
+            self.cursor = cmp::min(max_cursor, self.cursor + n as usize);
+        } else if n < 0 {
+            self.cursor = cmp::max(min_cursor, self.cursor - n as usize);
+        }
     }
 
-    fn move_hor(&mut self, n: usize) {
-    }
-    fn move_ver(&mut self, n: usize) {
+    fn move_ver(&mut self, n: i32) {
+        let max_lines = self.lines.len() as i32;
+        self.curr_line = cmp::max(0, cmp::min(max_lines, self.curr_line as i32 + n)) as usize; 
+        self.cursor = self.lines[self.curr_line].0;
     }
 
     fn move_eol(&mut self) {
-        self.cursor = self.lines[self.curr_line].0 + self.lines[self.curr_line].1;
+        self.cursor = self.lines[self.curr_line].0 + self.lines[self.curr_line].1 - 1;
     }
 
-    fn insert(&mut self, newchar: &char) {
+    fn insert(&mut self, newchar: char) {
         self.adjust_buffer();
+        if newchar == '\n' {
+            self.curr_line += 1;
+            self.lines.push((self.cursor+1, 0));
+        } else {
+            self.pre.push(newchar);
+            self.lines[self.curr_line].1 += 1;
+            self.cursor += 1;
+        }
     }
 
     fn insert_word(&mut self, newword: &str) {
         self.adjust_buffer();
+        let mut curr_len = 0;
+        let mut start = self.cursor;
+        if newword.trim().len() > 0 {
+            for ch in newword.chars() {
+                if ch == '\n' {
+                    self.lines.push((start, curr_len));
+                    self.cursor += curr_len;
+                    self.curr_line += 1;
+                    start = self.cursor + 1;
+                    curr_len = 0                 
+                }
+                else {
+                self.pre.push(ch);
+                curr_len += 1;
+                }
+            }
+        }
     }
 
     fn insert_line(&mut self, newline: &str) {
         self.adjust_buffer();
-        let linelen = newline.len();
-        if linelen > 0 {
-            self.lines.push((self.cursor, linelen));
+        let mut curr_len = 0;
+        let mut start = self.cursor;
+        if newline.trim().len() > 0 {
             for ch in newline.chars() {
-                self.pre.push(ch);
+                if ch == '\n' {
+                    self.lines.push((start, curr_len));
+                    self.cursor += curr_len;
+                    self.curr_line += 1;
+
+                    start = self.cursor + 1;
+                    curr_len = 0                 
+                } else {
+                    self.pre.push(ch);
+                    curr_len += 1
+                }
             }
-            self.cursor += linelen;
-            self.curr_line += 1    
+            if curr_len > 0 {
+                self.lines.push((start, curr_len));
+                self.cursor += curr_len;
+                self.curr_line += 1    
+            }
         }
+    }
+
+    fn backspace(&mut self) {
+        if self.cursor > self.lines[self.curr_line].0 {
+            self.pre.pop();
+            self.cursor -= 1;
+            self.lines[self.curr_line].1 -= 1
+        } 
+    }
+
+    fn delete(&mut self) {
+        if self.cursor < self.lines[self.curr_line].0 + self.lines[self.curr_line].1 {
+            self.post.pop();
+            self.lines[self.curr_line].1 -= 1
+        }
+    }
+
+    fn delete_line(&mut self) {
     }
 
     fn show(&mut self) {
@@ -120,9 +182,18 @@ impl SubEditor {
                 let end = self.post.len() - (self.lines[ind].0 - self.cursor);
                 currline = self.post[end-self.lines[ind].1 .. end].iter().rev().collect();
             } else {
-                let preline:String = self.pre[self.lines[ind].0 .. self.cursor].iter().collect();
-                let start = self.post.len() - self.lines[ind].1 + (self.cursor - self.lines[ind].0);
-                let postline:String = self.post[start .. self.post.len()].iter().rev().collect();
+                let preline:String = if self.cursor > self.lines[ind].0 {
+                    self.pre[self.lines[ind].0 .. self.cursor].iter().collect()
+                } else {
+                    String::new()
+                };
+
+                let start = self.post.len() + (self.cursor - self.lines[ind].0) - self.lines[ind].1;
+                let postline:String = if self.post.len() > start {
+                    self.post[start .. self.post.len()].iter().rev().collect()
+                } else {
+                    String::new()
+                };
                 currline = format!("{}{}",preline,postline);
             }
             println!("{}| {:?}",ind, currline);
@@ -136,9 +207,12 @@ fn main() {
     editor.open("/home/manan/Projects/rust-editor/files/test1.txt");
     editor.show();
 
-    editor.move_d();
-    for _ in 0..5 {
-        editor.move_r();
-    }
+    editor.move_ver(1);
+    editor.move_hor(5);
+    editor.insert('w');
+    editor.show();
+    
+    editor.move_hor(-1);
+    editor.insert_word("Manan");
     editor.show();
 }
