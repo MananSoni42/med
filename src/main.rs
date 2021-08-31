624da7e7a9630d4b9f6da953546ef6a71e357b5d
manan <manansoni1399@gmail.com>
2021-08-31 21:50:00 +0530

    switch to gap buffer, write basic backend

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use std::fs::File;
 use std::path::Path;
 use std::cmp;
-use std::io::{self, stdout, Write, BufRead};
+use std::io::{self, BufRead, Write, stdout};
+use std::time::Duration;
 use crossterm::{
-    execute,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    ExecutableCommand, Result,
-    event,
+    cursor::{self,position},
+    event::{poll, read, DisableMouseCapture, EnableMouseCapture, Event, KeyCode, KeyEvent, KeyModifiers },
+    execute, queue, style,
+    terminal::{self, ClearType},
+    Command, ExecutableCommand, Result
 };
 
 // The output is wrapped in a Result to allow matching on errors
@@ -19,22 +21,18 @@ where P: AsRef<Path>, {
 
 #[derive(Debug)]
 struct SubEditor {
-    cursor: usize,
-    curr_line: usize,
-    pre: Vec<char>,
-    post: Vec<char>,
-    lines: Vec<(usize, usize)>
+    pre: usize,
+    post: usize,
+    text: Vec<char>
 }
 
 impl SubEditor {
 
     fn init() -> SubEditor {
         SubEditor {
-            cursor: 0,
-            curr_line: 0,
-            pre: Vec::new(),
-            post: Vec::new(),
-            lines: Vec::new(),  
+            pre: 0,
+            post: 32-1,
+            text: vec!['\0'; 32]
         }
     }
 
@@ -46,173 +44,107 @@ impl SubEditor {
             Ok(lines) => lines,            
         };
 
+        let mut i = 1;
         for line in lines {
             match line {
-                Ok(content) => self.insert_line(&content),
-                Err(_) => ()
+                Ok(content) => { 
+                    for ch in content.chars() {
+                        self.insert(ch);
+                    } self.insert('\n'); 
+                    i += 1;
+                },
+                Err(_) => panic!("Could not read Line {} in file {}", i+1, path.display()),
             }
-        } 
-        self.cursor = 0;
-        self.curr_line = 0;
-        self.adjust_buffer(); 
+        } self.move_start(); 
     }
 
     fn adjust_buffer(&mut self) {
-        let buff_cursor = self.pre.len();
-        if self.cursor == buff_cursor { 
-            return; 
-        }
-        else {
-            let deficit = if self.cursor > buff_cursor { self.cursor - buff_cursor } else { buff_cursor - self.cursor };
-            for _ in 0..deficit {
-                if self.cursor < buff_cursor {
-                    self.post.push(self.pre.pop().unwrap());
-                } else {
-                    self.pre.push(self.post.pop().unwrap());
-                }
+        let free = self.post + 1 - self.pre;
+        let cap = self.text.len();
+
+        if free <= 4 {
+            self.text.resize(2*cap, '\0');
+            let post_len = cap-1-self.post;
+            for i in 0..post_len {
+                self.text[2*cap-1-i] = self.text[cap-1-i];
             }
+            self.post += cap;
+
+        } 
+        else if free > 32 && free as f32 > 0.75*self.text.len() as f32 {
+            let post_len = cap-1-self.post;
+            let mut post_text: Vec<char> = Vec::with_capacity(post_len);
+            
+            for i in 0..post_len {
+                post_text[i] = self.text[cap-1-i];
+            } 
+
+            self.text.resize(cap/2, '\0');
+            
+            for i in 0..post_len {
+                self.text[cap/2-1-i] = post_text[i];
+            }
+            self.post -= cap/2
         }
-    }
 
-    fn move_hor(&mut self, n: i32) {
-        let min_cursor = self.lines[self.curr_line].0;
-        let max_cursor = min_cursor + self.lines[self.curr_line].1;
-        if n > 0 {
-            self.cursor = cmp::min(max_cursor, self.cursor + n as usize);
-        } else if n < 0 {
-            self.cursor = cmp::max(min_cursor, self.cursor - n as usize);
-        }
-    }
-
-    fn move_ver(&mut self, n: i32) {
-        let max_lines = self.lines.len() as i32;
-        self.curr_line = cmp::max(0, cmp::min(max_lines, self.curr_line as i32 + n)) as usize; 
-        self.cursor = self.lines[self.curr_line].0;
-    }
-
-    fn move_eol(&mut self) {
-        self.cursor = self.lines[self.curr_line].0 + self.lines[self.curr_line].1 - 1;
     }
 
     fn insert(&mut self, newchar: char) {
         self.adjust_buffer();
-        if newchar == '\n' {
-            self.curr_line += 1;
-            self.lines.push((self.cursor+1, 0));
-        } else {
-            self.pre.push(newchar);
-            self.lines[self.curr_line].1 += 1;
-            self.cursor += 1;
+        self.text[self.pre] = newchar;
+        self.pre += 1;
+    }
+
+    fn backspace(&mut self) -> bool {
+        if self.pre > 0 {
+            self.pre -= 1;
+            return true;
+        } return false;
+    }
+
+    fn move_l(&mut self) -> bool {
+        if self.pre > 0 {
+            self.pre -= 1;
+            self.text[self.post] = self.text[self.pre];
+            self.post -= 1;
+            return true;
+        }
+        return false;
+    }
+
+    fn move_r(&mut self) -> bool {
+        if self.post + 1 < self.text.len() {
+            self.post += 1;
+            self.text[self.pre] = self.text[self.post];
+            self.pre += 1;
+            return true;
+        }
+        return false;
+    }
+
+    fn move_start(&mut self) {
+        while self.pre > 0 {
+            self.move_l();
         }
     }
 
-    fn insert_word(&mut self, newword: &str) {
-        self.adjust_buffer();
-        let mut curr_len = 0;
-        let mut start = self.cursor;
-        if newword.trim().len() > 0 {
-            for ch in newword.chars() {
-                if ch == '\n' {
-                    self.lines.push((start, curr_len));
-                    self.cursor += curr_len;
-                    self.curr_line += 1;
-                    start = self.cursor + 1;
-                    curr_len = 0                 
-                }
-                else {
-                self.pre.push(ch);
-                curr_len += 1;
-                }
-            }
+    fn move_end(&mut self) {
+        while self.post < self.text.len() {
+            self.move_r();
         }
-    }
-
-    fn insert_line(&mut self, newline: &str) {
-        self.adjust_buffer();
-        let mut curr_len = 0;
-        let mut start = self.cursor;
-        if newline.trim().len() > 0 {
-            for ch in newline.chars() {
-                if ch == '\n' {
-                    self.lines.push((start, curr_len));
-                    self.cursor += curr_len;
-                    self.curr_line += 1;
-
-                    start = self.cursor + 1;
-                    curr_len = 0                 
-                } else {
-                    self.pre.push(ch);
-                    curr_len += 1
-                }
-            }
-            if curr_len > 0 {
-                self.lines.push((start, curr_len));
-                self.cursor += curr_len;
-                self.curr_line += 1    
-            }
-        }
-    }
-
-    fn backspace(&mut self) {
-        if self.cursor > self.lines[self.curr_line].0 {
-            self.pre.pop();
-            self.cursor -= 1;
-            self.lines[self.curr_line].1 -= 1
-        } 
-    }
-
-    fn delete(&mut self) {
-        if self.cursor < self.lines[self.curr_line].0 + self.lines[self.curr_line].1 {
-            self.post.pop();
-            self.lines[self.curr_line].1 -= 1
-        }
-    }
-
-    fn delete_line(&mut self) {
-    }
-
-    fn show(&mut self) {
-        self.adjust_buffer();
-        println!("--- Med: v0.1 ---");
-        for (ind,line) in self.lines.iter().enumerate() {
-            let mut currline = String::new();
-            if ind < self.curr_line {
-                currline = self.pre[self.lines[ind].0 .. self.lines[ind].0+self.lines[ind].1].iter().collect();
-            } else if ind > self.curr_line {
-                let end = self.post.len() - (self.lines[ind].0 - self.cursor);
-                currline = self.post[end-self.lines[ind].1 .. end].iter().rev().collect();
-            } else {
-                let preline:String = if self.cursor > self.lines[ind].0 {
-                    self.pre[self.lines[ind].0 .. self.cursor].iter().collect()
-                } else {
-                    String::new()
-                };
-
-                let start = self.post.len() + (self.cursor - self.lines[ind].0) - self.lines[ind].1;
-                let postline:String = if self.post.len() > start {
-                    self.post[start .. self.post.len()].iter().rev().collect()
-                } else {
-                    String::new()
-                };
-                currline = format!("{}{}",preline,postline);
-            }
-            println!("{}| {:?}",ind, currline);
-        }
-        println!("-----------------\n");
     }
 }
 
 fn main() {
     let mut editor = SubEditor::init();
     editor.open("/home/manan/Projects/rust-editor/files/test1.txt");
-    editor.show();
+    for ch in "manan1".chars() {
+        editor.insert(ch);
+    }
+    editor.backspace();
+    editor.move_r();
 
-    editor.move_ver(1);
-    editor.move_hor(5);
-    editor.insert('w');
-    editor.show();
-    
-    editor.move_hor(-1);
-    editor.insert_word("Manan");
-    editor.show();
+    println!("{:?}", editor.text);
+    println!("({}, {}), {} / {}", editor.pre, editor.post, editor.pre + editor.text.len() - 1 - editor.post, editor.text.len());
 }
+
