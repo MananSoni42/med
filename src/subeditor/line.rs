a7e711f07cdc249dd13a7e511c2313783a6545a9
manan <manansoni1399@gmail.com>
2021-09-01 14:14:13 +0530

    Add support for lines, implement moving up,down

---
+++ src/subeditor/line.rs
@@ -0,0 +1,120 @@
+#[derive(Debug)]
+pub struct Line {
+    pre: usize,
+    post: usize,
+    text: Vec<char>
+}
+
+impl Line {
+
+    pub fn init() -> Line {
+        Line {
+            pre: 0,
+            post: 32-1,
+            text: vec!['\0'; 32]
+        }
+    }
+
+    pub fn init_with_line(newline: String) -> Line {
+        let mut len = 32;
+        while len < newline.len() { len*= 2; }
+        let mut text: Vec<char> = vec!['\0'; len];
+        let offset = len - newline.len();
+        for (i,ch) in newline.chars().enumerate() {
+            text[offset+i] = ch;
+        }
+
+        Line {
+            pre: 0,
+            post: offset-1,
+            text: text
+        }
+    }
+
+    pub fn adjust_buffer(&mut self) {
+        let free = self.post + 1 - self.pre;
+        let cap = self.text.len();
+
+        if free <= 4 {
+            self.text.resize(2*cap, '\0');
+            let post_len = cap-1-self.post;
+            for i in 0..post_len {
+                self.text[2*cap-1-i] = self.text[cap-1-i];
+            }
+            self.post += cap;
+
+        } 
+        else if free > 32 && free as f32 > 0.75*self.text.len() as f32 {
+            let post_len = cap-1-self.post;
+            let mut post_text: Vec<char> = Vec::with_capacity(post_len);
+            
+            for i in 0..post_len {
+                post_text[i] = self.text[cap-1-i];
+            } 
+
+            self.text.resize(cap/2, '\0');
+            
+            for i in 0..post_len {
+                self.text[cap/2-1-i] = post_text[i];
+            }
+            self.post -= cap/2
+        }
+
+    }
+
+    pub fn insert(&mut self, newchar: char) {
+        self.adjust_buffer();
+        self.text[self.pre] = newchar;
+        self.pre += 1;
+    }
+
+    pub fn backspace(&mut self) -> bool {
+        if self.pre > 0 {
+            self.pre -= 1;
+            return true;
+        } return false;
+    }
+
+    pub fn move_l(&mut self) -> bool {
+        if self.pre > 0 {
+            self.pre -= 1;
+            self.text[self.post] = self.text[self.pre];
+            self.post -= 1;
+            return true;
+        }
+        return false;
+    }
+
+    pub fn move_r(&mut self) -> bool {
+        if self.post + 1 < self.text.len() {
+            self.post += 1;
+            self.text[self.pre] = self.text[self.post];
+            self.pre += 1;
+            return true;
+        }
+        return false;
+    }
+
+    pub fn move_start(&mut self) {
+        while self.pre > 0 {
+            self.move_l();
+        }
+    }
+
+    pub fn move_end(&mut self) {
+        while self.post < self.text.len() {
+            self.move_r();
+        }
+    }
+
+    pub fn show(&self, i: usize) {
+        let mut line = String::new();
+        line.push_str(&i.to_string());
+        line.push_str(" | `");
+        for i in 0..self.pre { line.push(self.text[i]); }
+        for i in self.post..self.text.len() { line.push(self.text[i]); }
+        line.push('`');
+        println!("{}", line);
+        //println!("{},{} / {} | {:?}",self.pre, self.post, self.text.len(), self.text)
+    }
+}    
---
+++ src/subeditor/mod.rs
@@ -0,0 +1,86 @@
+use deepmesa::lists::{LinkedList,linkedlist::Node};
+use std::fs::File;
+use std::path::Path;
+use std::io::{self,BufRead,Write};
+
+mod line;
+use line::Line;
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[derive(Debug)]
+pub struct SubEditor {
+    curr_line_num: usize,
+    curr_line: Option<Node<Line>>,
+    lines: LinkedList::<Line>
+}
+
+impl SubEditor {
+    pub fn open(path: &str) -> SubEditor {
+
+        let mut subed = SubEditor {
+            curr_line_num: 0,
+            curr_line: None,
+            lines: LinkedList::<Line>::with_capacity(8)
+        };
+
+        let path = Path::new(path);
+
+        let lines = match read_lines(&path) {
+            Err(why) => panic!("couldn't open {}: {}", path.display(), why),
+            Ok(lines) => lines,            
+        };
+
+        for line in lines {
+            match line {
+                Ok(content) => { 
+                    subed.curr_line_num += 1;
+                    subed.curr_line = Some(subed.lines.push_tail(Line::init_with_line(content)));
+                },
+                Err(_) => panic!("Could not read Line {} in file {}", subed.curr_line_num, path.display()),
+            }
+        }
+
+        return subed;
+    }
+
+    pub fn move_l(&mut self) {
+        let lineref = self.curr_line.clone().unwrap();
+        let line = self.lines.node_mut(&lineref).unwrap();
+        line.move_l();
+    }
+
+    pub fn move_r(&mut self) {
+        let lineref = self.curr_line.clone().unwrap();
+        let line = self.lines.node_mut(&lineref).unwrap();
+        line.move_r();
+    }
+
+    pub fn move_u(&mut self) {
+        let lineref = self.curr_line.clone().unwrap();
+        match self.lines.prev_node(&lineref) {
+            Some(v) => self.curr_line = Some(v),
+            None => () 
+        }
+    }
+ 
+    pub fn move_d(&mut self) {
+        let lineref = self.curr_line.clone().unwrap();
+        match self.lines.next_node(&lineref) {
+            Some(v) => self.curr_line = Some(v),
+            None => () 
+        }
+    }
+
+    pub fn show(&self) {
+        for (i,line) in self.lines.iter().enumerate() {
+            line.show(i);
+        }
+    }
+}    
